262728d41488f81217417f86ae7bbf0b2a6751c0
chengzihao <2603232854@qq.com>
2023-03-06 14:57:37 +0800

    Git,这是在hot-fix中的修改

--- src/main/java/com/itcast/reggie/utils/git.java
+++ src/main/java/com/itcast/reggie/utils/git.java
@@ -4,5 +4,7 @@ public class git {
     public static void main(String[] args) {
         System.out.println("我刚刚被修改了");
         System.out.println("我又被修改了");
+
+        System.out.println("w我又一次修改咯");
     }
 }
