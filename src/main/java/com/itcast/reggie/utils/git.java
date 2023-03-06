e316ebc821acc7e4849ef909bae5355ccd60d059
chengzihao <2603232854@qq.com>
2023-03-06 14:52:27 +0800

    Git,第二次修改了

--- src/main/java/com/itcast/reggie/utils/git.java
+++ src/main/java/com/itcast/reggie/utils/git.java
@@ -3,5 +3,6 @@ package com.itcast.reggie.utils;
 public class git {
     public static void main(String[] args) {
         System.out.println("我刚刚被修改了");
+        System.out.println("我又被修改了");
     }
 }
